5a11c5d356ca236cf6ac84975c570d6a979dd0a1
Dmitryj <gudrummers@gmail.com>
2015-12-01 08:33:38 -0500

    3 commit

---
+++ chef-mysql_jboss/metadata.rb
@@ -0,0 +1,8 @@
+name 'chef-mysql_jboss'
+maintainer 'Dmitryj'
+maintainer_email 'gudrumms@gmail.com'
+license 'all_rights'
+description 'Installs/Configures chef-mysql_jboss'
+long_description 'Installs/Configures chef-mysql_jboss'
+version '0.1.0'
+depends 'apt', '~> 2.9.2'
